6fc58a0096d49baaf469e7f32b076f64b3622860
Kristina Yarovinsky <ky226@georgetown.edu>
2021-05-15 00:21:09 -0400

    Dividing into 3 timeframes, printing out results

--- testing_screener.py
+++ testing_screener.py
@@ -9,29 +9,56 @@ filters = Screener.load_filter_dict()
 
 print("Welcome to the program!")
 
-timeframe = input("What is your timeframe?")
+timeframe = input("What is your timeframe? Please enter 1, 2, or 3 depending on how many months you intend to hold: ")
+
+if timeframe == "1" or timeframe == "2" or timeframe == "3":
+    print("Thank you. You have indicated that you plan to invest for ", timeframe, "months.")
+else:
+    print("That is not a valid input. We will proceed with a default value of 3 months.")
+    timeframe = "3"
+    
+
+amount = input("How much money do you plan to invest? $")
+
 
 if timeframe == "1":
     some_filters = [filters["P/E"]["Low (<15)"], filters["Market Cap."]["Mid ($2bln to $10bln)"], filters["Target Price"]["30% Above Price"]]
-    stock_list = Screener(filters=some_filters, order="ticker")
-    #print(stock_list)
-    stock_list.to_csv("test_stocks.csv")
+    
+elif timeframe == "2":
 
-    print("Retrieving stock data...")
-    stock_data = stock_list.get_ticker_details()
-    #print(stock_data)
+    some_filters = [filters["P/E"]["Low (<15)"], filters["Market Cap."]["Mid ($2bln to $10bln)"], filters["Target Price"]["30% Above Price"]]
+elif timeframe == "3":
 
-    print("We have processed the data and recommend the following securities:")
-    for i in stock_data:
-        print(i["Ticker"])
-
-    proceed = input("Would you like to proceed? (Y/N)")
-
+    some_filters = [filters["P/E"]["Low (<15)"], filters["Market Cap."]["Mid ($2bln to $10bln)"], filters["Target Price"]["30% Above Price"]]
 
 else:
-    print("2")
+    print("Try again.")
 
+stock_list = Screener(filters=some_filters, order="ticker")
+#print(stock_list)
+stock_list.to_csv("test_stocks.csv")
 
+print("Retrieving stock data...")
+stock_data = stock_list.get_ticker_details()
+#print(stock_data)
+
+#counter variable to determine how many options there are
+counter = 0
+
+print("We have processed the data and recommend the following securities:")
+for i in stock_data:
+    print(i["Ticker"])
+    counter = counter + 1
+
+proceed = input("Please type 'Yes' if you wish to proceed. Type 'No' if you would like to quit.")
+
+if proceed == "Yes" or proceed == "yes":
+    print("We will now run through each of the recommended stocks, and you may indicate if you would liske to include it in your portfolio or not.")
+    print("Based on the amount of money that you are willing to invest, we would recommend selecting 5 of the ", counter, "recommended stocks/")
+
+    #proceed with the program
+else:
+    print("Thank you for using the program. Goodbye.")
 
 
 #####
