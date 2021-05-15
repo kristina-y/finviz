e920962c57982da1b864530233bdb2f0536e4c18
Kristina Yarovinsky <ky226@georgetown.edu>
2021-05-15 00:29:57 -0400

    commenting and adding data validation

--- testing_screener.py
+++ testing_screener.py
@@ -6,21 +6,34 @@ from finviz.screener import Screener
 # filters dict contains the corresponding filter tags
 filters = Screener.load_filter_dict()
 
-
+# Welcome message:
 print("Welcome to the program!")
 
+# Determining investment timeframe (which will cause the algorithm to vary)
 timeframe = input("What is your timeframe? Please enter 1, 2, or 3 depending on how many months you intend to hold: ")
 
+# Data validation for timeframe
 if timeframe == "1" or timeframe == "2" or timeframe == "3":
     print("Thank you. You have indicated that you plan to invest for ", timeframe, "months.")
 else:
     print("That is not a valid input. We will proceed with a default value of 3 months.")
     timeframe = "3"
     
-
+# Figuring out how much $ the user wishes to invest. This will determine the recommendation of how many investments to select
 amount = input("How much money do you plan to invest? $")
 
+#Data validation for amount
+if float(amount) < 1000000000 and float(amount) > 0:
+    print("You have entered that you wish to invest $", amount, ".")
+    amount = float(amount)
+else:
+    print("Your entry is not valid. We will proceed with a default value of $1,000")
+    amount = 1000
 
+# Telling the user that data will be processed now.
+print("We will now process your data and recommend a list of securities.")
+
+# Determining proper filters based on the timeframe.
 if timeframe == "1":
     some_filters = [filters["P/E"]["Low (<15)"], filters["Market Cap."]["Mid ($2bln to $10bln)"], filters["Target Price"]["30% Above Price"]]
     
@@ -35,26 +48,34 @@ else:
     print("Try again.")
 
 stock_list = Screener(filters=some_filters, order="ticker")
-#print(stock_list)
+
+# Creating a csv with the stock recommendations
+print("Downloading csv file with the stock recommendations...")
 stock_list.to_csv("test_stocks.csv")
 
+# Stock_data is a list that will hold the recommendations.
 print("Retrieving stock data...")
 stock_data = stock_list.get_ticker_details()
-#print(stock_data)
 
 #counter variable to determine how many options there are
 counter = 0
 
+# Printing out the tickers of the recommended equities.
 print("We have processed the data and recommend the following securities:")
 for i in stock_data:
     print(i["Ticker"])
     counter = counter + 1
 
-proceed = input("Please type 'Yes' if you wish to proceed. Type 'No' if you would like to quit.")
+# User may either proceed or quit the program
+proceed = input("Please type 'Yes' if you wish to proceed. Type anything else if you would like to quit. ")
 
+# User chose to proceed
 if proceed == "Yes" or proceed == "yes":
-    print("We will now run through each of the recommended stocks, and you may indicate if you would liske to include it in your portfolio or not.")
-    print("Based on the amount of money that you are willing to invest, we would recommend selecting 5 of the ", counter, "recommended stocks/")
+    print("We will now run through each of the recommended stocks, and you may indicate if you would like to include it in your portfolio or not.")
+    
+    #Giving the user a recommendation for how many equities to invest in, if the amount is less than 1000.
+    if (amount < 1000):
+        print("Based on the amount of money that you are willing to invest, we would recommend selecting 5 of the ", counter, "recommended stocks/")
 
     #proceed with the program
 else:
